7ca43f8648083cf0a803638b58bd59c21d3516b8
matheusfrancisco <mfranciscobatistamachado@gmail.com>
2019-01-30 16:12:16 -0200

    open folder e files"

--- Projects/javascript/Dropbox-clone/app/routes/index.js
+++ Projects/javascript/Dropbox-clone/app/routes/index.js
@@ -6,40 +6,81 @@ var fs = require('fs');
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/file', (req, res) => {
 
-router.delete('/file', (req,res)=>{
+  let path = './' + req.query.path;
+
+  if (fs.existsSync(path)) {
+
+    fs.readFile(path, (err, data) => {
+
+      if (err) {
+
+        console.error(err);
+        res.status(400).json({
+          error: err
+        });
+
+      } else {
+
+        res.status(200).end(data);
+
+      }
+
+    });
+
+  } else {
+
+    res.status(404).json({
+      error: 'File not found.'
+    });
+
+  }
+
+});
+
+router.delete('/file', (req, res) => {
 
   let form = new formidable.IncomingForm({
     uploadDir: './upload',
-    keepExtension: true
+    keepExtensions: true
   });
 
-
-  //receive fields e files 
   form.parse(req, (err, fields, files) => {
 
-    let path = "./" + fields.path;
+    let path = './' + fields.path;
 
-    if(fs.existsSync(fields.path)){
+    if (fs.existsSync(path)) {
 
-      fs.unlink(path, err=>{
+      fs.unlink(path, err => {
 
-        if(err){
-          res.status(400).jsonp({
+        if (err) {
+
+          res.status(400).json({
             err
           });
+
         } else {
+
           res.json({
             fields
           });
+
         }
 
       });
+
+    } else {
+
+      res.status(404).json({
+        error: 'File not found.'
+      });
+
     }
-    
 
   });
-})
+
+});
 
 router.post('/upload', (req, res)=>{
 
