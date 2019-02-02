92e737922a818ca415f0492fa0b9a2613d2f6a7d
matheusfrancisco <mfranciscobatistamachado@gmail.com>
2019-02-02 10:04:22 -0200

    Create open panelAddContact and EditProfile !

--- Projects/javascript/whatsapp-clone/src/controller/WhatsAppController.js
+++ Projects/javascript/whatsapp-clone/src/controller/WhatsAppController.js
@@ -87,29 +87,49 @@ class WhatsAppController{
     initEvents(){
 	
 	    this.el.myPhoto.on('click', e=>{
-	    
-		this.el.panelEditProfile.addClass('open');
+            
+            this.closeAllLeftPanel();
+            this.el.panelEditProfile.show();
+            setTimeout(() => {
 
+                this.el.panelEditProfile.addClass('open');
+
+            }, 300);
 	    });
             
 	    this.el.btnNewContact.on('click', e=>{
-	    
-		this.el.paneladdContact.addClass('open');
+            
+            this.closeAllLeftPanel();
+            this.el.panelAddContact.show();
+
+            setTimeout(() => {
+                
+                this.el.panelAddContact.addClass('open');
+
+            }, 300);
 
 	    });
        
 	    this.el.btnClosePanelEditProfile.on('click', e=>{
 
-	   	this.el.panelEditProfile.removeClass('open'); 
+            this.el.panelEditProfile.removeClass('open'); 
 	    
-	    });
+        });
+        
+        this.el.btnClosePanelAddContact.on('click', e=>{
+            
+            this.el.panelAddContact.removeClass('open');
 
-
+        })
 
     }
 
     closeAllLeftPanel(){
-    
+        
+        this.el.panelAddContact.hide();
+        this.el.panelEditProfile.hide();
+
+
     }
 }
 
